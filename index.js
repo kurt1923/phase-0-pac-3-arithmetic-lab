f454d164198733d225f9eee255f96eab83df3b2e
Kurt Vermillion <kurtv0727@gmail.com>
2022-06-11 14:27:21 -0400

    commit tree e1386f9e531dcb0221e4a43fceacea2cb181ab9f

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-function add() {
-  }
-function subtract(){
-    }
+function add() {}
+function subtract(){}
 function multiply(){}
 function divide(){}
+function add(a,b){
+    return (a+b)}
+function subtract(a,b){
+    return (a-b)}    
+function multiply(a,b){
+    return (a*b)}
+function divide(a,b){
+    return (a/b)}
+function increment(number) {
+    return (++number);
+}
+function decrement(number) {
+    return (--number)
+}
+function makeInt(number) {
+    return parseInt(number, 10)
+}
+function preserveDecimal(string){
+    return parseFloat(string, 10)
+}
